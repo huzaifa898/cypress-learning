b2d7114d9efbd94937763dbbc4f6d297e3ed7b50
Huzaifa Tariq <122553707+huzaifa898@users.noreply.github.com>
2025-08-18 09:55:20 +0500

    add drop down

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,10 +1,14 @@
 describe("Login Functionality", () => { 
-    it('Login Registe user', () => {
+    it('Login Register user', () => {
         cy.visit('https://parabank.parasoft.com/parabank/index.htm;jsessionid=0653221BECE16D32949F8CAE891B51CA')
         //cy.get('#loginPanel > :nth-child(3) > a').click()
         cy.get(" input[name='username']").type('johndoe333')
         cy.get("input[name='password']").type('password123')  
         cy.get("input[value='Log In']").click()
+        cy.get("a[href='activity.htm?id=18672']").click()
+        cy.get("#month").select('January')
+        cy.get("#transactionType").select("All")
+        cy.get("input[value='Go']").click()
     })
 }) 
 
